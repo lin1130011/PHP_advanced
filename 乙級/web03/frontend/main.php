95f510db6ca6a75b6025e5a3325e4fa3413c26c4
lin1130011 <lin1130011@gmail.com>
2024-07-15 09:40:24 +0800

    更新乙級第三題main外觀

--- 乙級/web03/frontend/main.php
+++ 乙級/web03/frontend/main.php
@@ -1,11 +1,27 @@
 <div class="half" style="vertical-align:top;">
     <h1>預告片介紹</h1>
     <div class="rb tab" style="width:95%;">
-        <div id="abgne-block-20111227">
-            <ul class="lists">
-            </ul>
-            <ul class="controls">
-            </ul>
+        <div id="posters">
+            <div class="lists">
+                <div class="poster">
+                    <img src="./images/03A07.jpg" alt="">
+                </div>
+                <div class="name">132</div>
+            </div>
+            <div class="controls">
+                <div class="control">
+                    <div class="btn-left"></div>
+                </div>
+                <div class="btns">
+                    <div class="btn">A</div>
+                    <div class="btn">B</div>
+                    <div class="btn">C</div>
+                    <div class="btn">D</div>
+                </div>
+                <div class="control">
+                    <div class="btn-right"></div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
